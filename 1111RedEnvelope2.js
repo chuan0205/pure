9ab2c52f4fe97d7fcfda85bd717f51e9a32bbac7
chuan0205 <64533225+chuan0205@users.noreply.github.com>
2021-11-10 16:09:13 +0800

    Update 1111RedEnvelope2.js

--- 1111RedEnvelope2.js
+++ 1111RedEnvelope2.js
@@ -13,7 +13,7 @@ https://u.jd.com/yt5HPfV
 
 let rebateCodes = ''
 
-const $ = new Env('双十一无门槛红包');
+const $ = new Env('双十一无门槛红包22');
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 const notify = $.isNode() ? require('./sendNotify') : '';
 const Faker = $.isNode() ? require('./sign_graphics_validate.js') : '';
